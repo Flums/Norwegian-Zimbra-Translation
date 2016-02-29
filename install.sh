32e295f81772b05380c599fbb67c6cf336134160
Philip Gabrielsen <philip@redpill-linpro.com>
2016-02-29 15:01:25 +0100

    Some more changes to the wget options

--- install.sh
+++ install.sh
@@ -3,26 +3,26 @@
 VERSION=$(sudo -u zimbra /opt/zimbra/bin/zmcontrol -v| awk '{ print $2 }' | awk -F. '{ print $1 }')
 
 cd /opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/messages/
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/AjxMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/I18nMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZaMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZbMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZhMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZmMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZtMsg_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZmSMS_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/AjxMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/I18nMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZaMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZbMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZhMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZmMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZtMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZmSMS_no.properties
 
 chown zimbra: *
 
 cd /opt/zimbra/jetty/webapps/zimbraAdmin/WEB-INF/classes/messages/
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZaMsg_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/messages/ZaMsg_no.properties
 
 chown zimbra: *
 
 cd /opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/keys/
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/AjxKeys_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/ZhKeys_no.properties
-wget -Nnv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/ZmKeys_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/AjxKeys_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/ZhKeys_no.properties
+wget -nv https://raw.githubusercontent.com/Flums/Norwegian-Zimbra-Translation/master/zimbra$VERSION/keys/ZmKeys_no.properties
 
 chown zimbra: *
